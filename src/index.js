c5e8315ec9ce0e113c27e530e67e20bcc3a04e50
samuelltorres <samuelltorres145@gmail.com>
2023-10-18 18:31:45 -0300

    :tada: Setup inicial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(3, 4);
+console.log(result);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b:number): number {
+	return a + b;
+}
+
+const result: number = sum(3, 4);
+console.log(result);
